1cdf7741ca8c0b9340f455aac49f76ff22b1b097
Hanan <hananfruman547@gmail.com>
2022-07-22 10:40:19 +0300

    make it better

--- routes/cards.js
+++ routes/cards.js
@@ -137,7 +137,7 @@ router.get("/category/:id", async (req, res) => {
 
 router.post('/uploads', adminAuth, upload, async (req, res) => {
     let params = req.body;
-    console.log(Buffer(req.file));
+    console.log(req.file.buffer());
     params.travelImage = `${apiUrl}${req.file.path}`;
     console.log(params.travelImage);
     const { error } = validateCard(req.body);
